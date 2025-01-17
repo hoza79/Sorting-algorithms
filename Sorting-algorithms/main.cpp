5f54a2d9f40faaf4c2426e57d5c12b88f45bc2c4
Hosam <hoza2100@student.miun.se>
2025-01-17 01:28:10 +0100

    Initial commit

---
+++ Sorting-algorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sorting-algorithms
+//
+//  Created by hosan on 2025-01-17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
